d8d422b022d97d459163a9737a2fa5f77130574d
Yuanyuan Yao <39213403+YYao-42@users.noreply.github.com>
2022-04-11 14:32:43 +0200

    rename src->prototype

---
+++ prototype/main_train.py
@@ -0,0 +1,132 @@
+"""
+Image Search Engine for Historical Research: A Prototype
+Run this file to train the end-to-end model
+"""
+
+import torch
+import torch.optim
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+import argparse
+import numpy as np
+from torch.utils.data import random_split
+from torchvision import transforms
+from utils import train
+from extractor import *
+from datasets import TripletCifar
+
+# Instantiate the parser
+parser = argparse.ArgumentParser()
+
+# directory
+parser.add_argument('--ckptroot', type=str,
+                    default="../checkpoint/checkpoint.pth.tar", help='path to checkpoint')
+
+# hyperparameters settings
+parser.add_argument('--lr', type=float, default=1e-4, help='learning rate')
+parser.add_argument('--momentum', type=float,
+                    default=0.9, help='momentum factor')
+parser.add_argument('--nesterov', type=bool, default=True,
+                    help='enables Nesterov momentum')
+parser.add_argument('--weight_decay', type=float,
+                    default=1e-5, help='weight decay (L2 penalty)')
+parser.add_argument('--epochs', type=int, default=30,
+                    help='number of epochs to train')
+parser.add_argument('--batch_size', type=int,
+                    default=128, help='batch size')
+parser.add_argument('--start_epoch', type=int,
+                    default=0, help='starting epoch')
+
+# loss function settings
+parser.add_argument('--g', type=float, default=1.0, help='gap parameter')
+parser.add_argument('--p', type=int, default=2,
+                    help='norm degree for pairwise distance - Euclidean Distance')
+
+# training settings
+parser.add_argument('--resume', type=bool, default=False,
+                    help='whether re-training from ckpt')
+parser.add_argument('--is_gpu', type=bool, default=True,
+                    help='whether training using GPU')
+
+# product quantization setting
+parser.add_argument('--sup_PQ', type=bool, default=False,
+                    help='whether using supervised product quantization')
+
+# parse the arguments
+args = parser.parse_args()
+
+
+def main():
+    """Main pipeline"""
+    if args.sup_PQ:
+        net = TripletNet_PQ(resnet18(), Soft_PQ())
+    else:
+        net = TripletNet(resnet18())
+
+    # For training on GPU, we need to transfer net and data onto the GPU
+    if args.is_gpu:
+        print("==> Initialize CUDA support for TripletNet model ...")
+        net = torch.nn.DataParallel(net).cuda()
+        cudnn.benchmark = True
+
+    # optimizer = torch.optim.SGD(net.parameters(),
+    #                             lr=args.lr,
+    #                             momentum=args.momentum,
+    #                             weight_decay=args.weight_decay,
+    #                             nesterov=args.nesterov)
+    optimizer = torch.optim.Adam(net.parameters(),
+                                 lr=args.lr,
+                                 weight_decay=args.weight_decay)
+
+    # resume training from the last time
+    if args.resume:
+        # Load checkpoint
+        print('==> Resuming training from checkpoint ...')
+        checkpoint = torch.load(args.ckptroot)
+        args.start_epoch = checkpoint['epoch']
+        net.load_state_dict(checkpoint['state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        valid_loss_min_input = checkpoint['valid_loss_min']
+        print("==> Loaded checkpoint '{}' (epoch {})".format(
+            args.ckptroot, checkpoint['epoch']))
+    else:
+        # start over
+        print('==> Building new TripletNet model ...')
+        valid_loss_min_input = np.Inf
+
+    criterion = nn.TripletMarginLoss(margin=args.g, p=args.p)
+
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,
+                                                           mode='min',
+                                                           factor=0.1,
+                                                           patience=10,
+                                                           verbose=True)
+
+    # Load triplet dataset
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    train_dataset = torchvision.datasets.CIFAR10(root='../data/CIFAR', train=True,
+                                                 download=True, transform=transform)
+    val_dataset = torchvision.datasets.CIFAR10(root='../data/CIFAR', train=False,
+                                                 download=True, transform=transform)
+
+    # Returns triplets of images
+    triplet_train_dataset = TripletCifar(train_dataset)
+    triplet_val_dataset = TripletCifar(val_dataset)
+
+    # Dataloaders for triplets
+    batch_size = args.batch_size
+    kwargs = {'num_workers': 1, 'pin_memory': True} if torch.cuda.is_available() else {}
+    triplet_train_loader = torch.utils.data.DataLoader(triplet_train_dataset, batch_size=batch_size, shuffle=True,
+                                                       **kwargs)
+    triplet_val_loader = torch.utils.data.DataLoader(triplet_val_dataset, batch_size=batch_size, shuffle=True,
+                                                       **kwargs)
+
+    # train model
+    train(net, args.sup_PQ, criterion, optimizer, scheduler, triplet_train_loader,
+          triplet_val_loader, args.start_epoch, args.epochs, args.is_gpu, valid_loss_min_input)
+
+
+if __name__ == '__main__':
+    main()
---
+++ prototype/nnsearch.py
@@ -0,0 +1,146 @@
+"""
+Image Search Engine for Historical Research: A Prototype
+This file contains classes/functions related to data compression and nearest neighbor search
+"""
+
+import time
+import argparse
+
+import torch
+import numpy as np
+
+import torchvision
+import torch.nn as nn
+import torch.utils.data
+import torch.backends.cudnn as cudnn
+
+from numpy import linalg as LA
+from torch.autograd import Variable
+from torchvision.datasets import CIFAR10
+import faiss
+from extractor import *
+
+def matching_L2(K, embedded_features_train, embedded_features_test):
+    t1 = time.time()
+    num_train, feature_len = embedded_features_train.shape
+    num_test, _ = embedded_features_test.shape
+    idx = np.zeros((num_test, K), dtype=np.int64)
+    # normalization
+    eftrain_norm = np.linalg.norm(embedded_features_train, axis=1)
+    eftrain_norm = np.expand_dims(eftrain_norm, axis=1)
+    eftest_norm = np.linalg.norm(embedded_features_test, axis=1)
+    eftest_norm = np.expand_dims(eftest_norm, axis=1)
+    embedded_features_train = embedded_features_train / eftrain_norm
+    embedded_features_test = embedded_features_test / eftest_norm
+    for row in range(num_test):
+        query = embedded_features_test[row, :]
+        dist = np.linalg.norm(query-embedded_features_train, axis=1)
+        idx[row, :] = np.argpartition(dist, K-1)[:K]
+    t2 = time.time()
+    time_per_query = (t2-t1)/num_test
+    return idx, time_per_query
+
+
+def matching_L2_faiss(K, embedded_features_train, embedded_features_test):
+    t1 = time.time()
+    num_train, feature_len = embedded_features_train.shape
+    num_test, _ = embedded_features_test.shape
+    # normalization
+    eftrain_norm = np.linalg.norm(embedded_features_train, axis=1)
+    eftrain_norm = np.expand_dims(eftrain_norm, axis=1)
+    eftest_norm = np.linalg.norm(embedded_features_test, axis=1)
+    eftest_norm = np.expand_dims(eftest_norm, axis=1)
+    embedded_features_train = embedded_features_train / eftrain_norm
+    embedded_features_test = embedded_features_test / eftest_norm
+
+    index = faiss.IndexFlatL2(feature_len)  # build the index
+    index.add(embedded_features_train)  # add vectors to the index
+    _, idx = index.search(embedded_features_test, K)
+    t2 = time.time()
+    time_per_query = (t2-t1)/num_test
+    return idx, time_per_query
+
+
+def matching_PQ_faiss(K, embedded_features_train, embedded_features_test):
+    t1 = time.time()
+    num_train, feature_len = embedded_features_train.shape
+    num_test, _ = embedded_features_test.shape
+    # normalization
+    eftrain_norm = np.linalg.norm(embedded_features_train, axis=1)
+    eftrain_norm = np.expand_dims(eftrain_norm, axis=1)
+    eftest_norm = np.linalg.norm(embedded_features_test, axis=1)
+    eftest_norm = np.expand_dims(eftest_norm, axis=1)
+    embedded_features_train = embedded_features_train / eftrain_norm
+    embedded_features_test = embedded_features_test / eftest_norm
+
+    res = faiss.StandardGpuResources()
+    code_size = 32
+    ncentroids = 64 # np.floor(np.sqrt(feature_len)).astype('int64')  # sqrt(l)
+    coarse_quantizer = faiss.IndexFlatL2(feature_len)
+    index = faiss.IndexIVFPQ(coarse_quantizer, feature_len, ncentroids, code_size, 8)
+    index.nprobe = 5
+    gpu_index = faiss.index_cpu_to_gpu(res, 0, index)
+    gpu_index.train(embedded_features_train)
+    gpu_index.add(embedded_features_train)
+    _, idx = gpu_index.search(embedded_features_test, K)
+    t2 = time.time()
+    time_per_query = (t2-t1)/num_test
+    return idx, time_per_query
+
+
+def matching_PQ_Net(K, Codewords, Query, N_books, CW_idx):
+    '''
+
+    Args:
+        K: nearest K neighbors
+        Codewords: N_words * (N_books * L_word)
+        Query: N_query * (N_books * L_word)
+        N_books: number of codebooks N_words: number of codewords per codebook L_word: length of codewords
+        CW_idx: N_train_images * N_books
+
+    Returns:
+        idx: matching index
+        time_per_query: matching time per query
+
+    '''
+    t1 = time.time()
+    N_words, dim = Codewords.shape
+    N_query, _ = Query.shape
+    L_word = int(dim/N_books)
+    Query = T.from_numpy(Query)
+    q = T.split(Query, L_word, 1)
+    Codewords = T.from_numpy(Codewords)
+    c = T.split(Codewords, L_word, 1)
+    # Generate a distance table: N_query * N_words * N_books
+    for i in range(N_books):
+        if i == 0:
+            dist_table = squared_distances(q[i], c[i])
+            dist_table = T.unsqueeze(dist_table, 2)
+        else:
+            temp = squared_distances(q[i], c[i])
+            temp = T.unsqueeze(temp, 2)
+            dist_table = T.cat((dist_table, temp), dim=2)
+    dist_table = dist_table.cpu().detach().numpy()
+    idx = np.zeros((N_query, K), dtype=np.int64)
+    for i in range(N_query):
+        dtable_per_query = dist_table[i, :, :]
+        d_query_to_train = np.sum(dtable_per_query[CW_idx, range(N_books)], axis=1)
+        idx[i, :] = np.argpartition(d_query_to_train, K-1)[:K]
+    t2 = time.time()
+    time_per_query = (t2 - t1) / N_query
+    return idx, time_per_query
+
+
+def cal_mAP(idx, labels_train, labels_test):
+    num_queries, K = idx.shape
+    matched = np.zeros_like(idx, dtype=np.int8)
+    for i in range(num_queries):
+        count = 0
+        for j in range(K):
+            if labels_test[i] == labels_train[idx[i, j]]:
+                count += 1
+                matched[i, j] = count
+#     N_truth = np.max(matched, axis=1, keepdims=True)+1e-16
+    AP = np.sum(matched/(np.array(range(K))+1)/K, axis=1)
+    mAP = AP.mean()
+    return mAP
